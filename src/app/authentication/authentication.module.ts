e5a973c2fd7e2182d16527f8783725aeda46b7c3
Máté Csicsek <mate.csicsek@innovitech.hu>
2023-12-11 13:06:59 +0100

    authentication

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
     declarations: [
+        LoginComponent,
+        RegistrationComponent
     ],
     imports: [AuthenticationRoutingModule, SharedModule],
     exports: [],
