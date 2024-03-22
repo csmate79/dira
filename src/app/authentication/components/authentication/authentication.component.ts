67e165e53f4f142191898aa69c7643e95d61fc27
Máté Csicsek <mate.csicsek@innovitech.hu>
2024-03-22 17:11:15 +0100

    auth

--- src/app/authentication/components/authentication/authentication.component.ts
+++ src/app/authentication/components/authentication/authentication.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { RegistrationComponent } from '../registration/registration.component';
 
 @Component({
     selector: 'app-authentication',
     templateUrl: './authentication.component.html',
-    styleUrls: ['./authentication.component.scss']
+    styleUrls: ['./authentication.component.scss'],
 })
 export class AuthenticationComponent {
-    constructor(private dialogRef: MatDialogRef<AuthenticationComponent>) { }
+    public selectedTabIndex: number = 0;
+
+    constructor(private dialogRef: MatDialogRef<AuthenticationComponent>) {}
 
     closeDialog() {
         this.dialogRef.close();
+    }
+
+    public tabChange(event: any) {
+        this.selectedTabIndex = event.index;
     }
 }
